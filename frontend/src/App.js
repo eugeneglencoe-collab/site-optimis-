ecda00b14be86926a0c5ff37f3e92cea6dcffb6f
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 11:51:05 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
-import AdsPage from './components/AdsPage';
-import ConvertTokens from './components/ConvertTokens';
-import About from './components/About';
+import StatsCard from './components/StatsCard';
+import VideoCard from './components/VideoCard';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Admin from './pages/Admin';
+import Convert from './pages/Convert';
+import Videos from './pages/Videos';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/ads" element={<AdsPage />} />
-        <Route path="/convert" element={<ConvertTokens />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/convert" element={<Convert />} />
         <Route path="/about" element={<About />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
   );
