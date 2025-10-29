98c19621bd36b1714d16bed6962ca47986159da1
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 13:55:40 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
@@ -11,5 +11,3 @@ function App() {
     </Router>
   );
 }
-
-export default App;
