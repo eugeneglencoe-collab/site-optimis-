a1f1d2769fad260000dbe16a3795f5aa90592de7
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 10:38:19 +0100

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Bienvenue sur Pubflix</h1>
+      <p>Gagnez de l'argent en regardant des vidéos publicitaires.</p>
+      <button>Inscription / Connexion</button>
+    </div>
+  );
+}
