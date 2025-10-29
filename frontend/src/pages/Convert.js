8387f98a9a241463b0e4852751ba02fd7ec722f1
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 13:22:37 +0100

    Update Convert.js

--- frontend/src/pages/Convert.js
+++ frontend/src/pages/Convert.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './Convert.css';
 
 export default function Convert() {
   const token = localStorage.getItem('token');
