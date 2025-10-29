623b4240a90eb0d2882db602768cc5c177827db2
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 10:39:30 +0100

    Create About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h1>À propos de Pubflix</h1>
+      <p>Notre mission : permettre à chacun de gagner de l'argent simplement en regardant des vidéos publicitaires.</p>
+    </div>
+  );
+}
