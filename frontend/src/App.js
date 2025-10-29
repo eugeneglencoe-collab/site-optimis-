ea90bd467211492b43f79d9bffcd426ab4f24e1f
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 12:36:18 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import DashboardPage from './components/DashboardPage';
-import StatsCard from './components/StatsCard';
-import VideoCard from './components/VideoCard';
-
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Videos from './pages/Videos';
+import Convert from './pages/Convert';
 import About from './pages/About';
 import Admin from './pages/Admin';
-import Convert from './pages/Convert';
-import Videos from './pages/Videos';
+
+import './style/main.css';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/videos" element={<Videos />} />
         <Route path="/convert" element={<Convert />} />
         <Route path="/about" element={<About />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
