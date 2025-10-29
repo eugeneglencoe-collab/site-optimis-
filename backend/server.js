d8249cc8f8799055071b3e3b990c7c2a79e0c3a7
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 10:31:24 +0100

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const authRoutes = require('./routes/auth');
+const videoRoutes = require('./routes/videos');
+const adminRoutes = require('./routes/admin');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/videos', videoRoutes);
+app.use('/api/admin', adminRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
