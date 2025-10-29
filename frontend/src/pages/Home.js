e60aa0cb7d8013db849c5b4bfb70bda1dfbfcba0
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 10:34:34 +0100

    Delete frontend directory

--- frontend/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="home">
-      <h1>Bienvenue sur Pubflix</h1>
-      <p>Gagnez de l'argent en regardant des vidéos publicitaires.</p>
-      <button>Inscription / Connexion</button>
-    </div>
-  );
-}
