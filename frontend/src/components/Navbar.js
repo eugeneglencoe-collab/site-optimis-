6d53df3f51549dc16def3d659f9921b96dbc670c
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 13:58:10 +0100

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h2>Pubflix</h2>
+      <div className="logo">PUBFLIX</div>
       <ul className="nav-links">
         <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/videos">Vidéos</Link></li>
+        <li><Link to="/videos">Publicités</Link></li>
+        <li><Link to="/dashboard">Tableau de bord</Link></li>
         <li><Link to="/convert">Convertir</Link></li>
         <li><Link to="/about">À propos</Link></li>
-        <li><Link to="/admin">Admin</Link></li>
       </ul>
     </nav>
   );
