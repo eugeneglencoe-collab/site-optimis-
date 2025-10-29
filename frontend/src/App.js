111ac97eff4fafe9ded2a0ab10630faf9ebbbf3d
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 13:43:21 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Videos from './pages/Videos';
-import Convert from './pages/Convert';
-import About from './pages/About';
-import Admin from './pages/Admin';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./style/main.css";
 
-import './style/main.css';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Videos from "./pages/Videos";
+import Convert from "./pages/Convert";
+import About from "./pages/About";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/videos" element={<Videos />} />
-        <Route path="/convert" element={<Convert />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/videos" element={<Videos />} />
+          <Route path="/convert" element={<Convert />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
