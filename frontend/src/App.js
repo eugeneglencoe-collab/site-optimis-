17ce3ed0b454ae29c44cbd331780c4321df31448
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2025-10-29 13:49:38 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./style/main.css";
-
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Videos from "./pages/Videos";
-import Convert from "./pages/Convert";
-import About from "./pages/About";
-import Admin from "./pages/Admin";
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/videos" element={<Videos />} />
-          <Route path="/convert" element={<Convert />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
